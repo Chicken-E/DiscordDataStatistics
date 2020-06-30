65d256a8802e26d9a4df581c77243ddeadecfd58
Chicken <45627874+Chicken-E@users.noreply.github.com>
2020-06-30 11:25:40 +0000

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,8 @@ const OUTPUT_TO_FILE = true;
 //variables with all the data
 let messages_full = []
 let messages_txt = "messageid,channelid,time,message\n"
-let attachements_txt = ""
+let attachments_full = []
+let attachments_txt = "messageid,channelid,time,attachments\n"
 
 //count for the most active channel
 let highest_count = 0
@@ -39,7 +40,14 @@ Object.keys(data).forEach(id=>{
                 message: Contents
             })
             //add attachment to list
-            attachements_txt += Attachments ? Attachments + "\n" : "";
+            if(Attachments) {
+                attachments_full.push({
+                    cid: id,
+                    msgid: ID,
+                    time: new Date(Timestamp).getTime(),
+                    attachments: Attachments
+                })
+            }
         })
         //test for the most active channel
         if(rows.length>highest_count) {
@@ -57,15 +65,17 @@ Object.keys(data).forEach(id=>{
 //wait till all files have been processed
 Promise.all(promises).then(v=>{
     //make printable data
-    messages_txt += messages_full.sort((a,b)=>a.time-b.time).map(m=>`${m.msgid},${m.cid},${m.time},${m.message.replace(/\n/g, " ")}`).join("\n")
+    messages_txt += messages_full.sort((a,b)=>a.time-b.time).map(m=>`${m.msgid},${m.cid},${m.time},${m.message}`).join("\n")
+    attachments_txt += attachments_full.sort((a,b)=>a.time-b.time).map(m=>`${m.msgid},${m.cid},${m.time},${m.attachments}`).join("\n")
 
     //if outputting to file output the extracted data to files
     if(OUTPUT_TO_FILE) {
         fs.writeFileSync("messages.csv", messages_txt, {encoding: "utf-8"})
-        fs.writeFileSync("attachements.txt", attachements_txt, {encoding: "utf-8"})
-        console.log("\x1b[0m\x1b[2m\x1b[34mOutputted messages and attachements to their own files.\x1b[0m")
-        console.log("\x1b[0m\x1b[2m\x1b[34mAttachements in \"attachements.txt\" seperated by newline (\\n). Sorted by some magic factor!\x1b[0m")
-        console.log("\x1b[0m\x1b[2m\x1b[34mMessages in \"messages.csv\" in columns of message id, channel id, timestamp, message. Sorted by timestamp.\x1b[0m")
+        fs.writeFileSync("attachments.csv", attachments_txt, {encoding: "utf-8"})
+        console.log("\x1b[0m\x1b[2m\x1b[34mOutputted messages and attachments to their own files.")
+        console.log("Attachments in \"attachments.csv\" in columns of message id, channel id, timestamp, attachments.")
+        console.log("Messages in \"messages.csv\" in columns of message id, channel id, timestamp, message.")
+        console.log("Both sorted by timestamp.\x1b[0m")
     }
 
     //output statistics (the weird chars are just color codes)
@@ -74,7 +84,7 @@ Promise.all(promises).then(v=>{
     console.log("")
     console.log("\x1b[0m\x1b[1m\x1b[33mTotal messages: \x1b[0m\x1b[36m" + total)
     console.log("\x1b[0m\x1b[1m\x1b[33mCharacters typed: \x1b[0m\x1b[36m" + messages_full.map(m=>m.message).join("").length)
-    console.log("\x1b[0m\x1b[1m\x1b[33mAttachements sent: \x1b[0m\x1b[36m" + (attachements_txt.split("\n").length-1))
+    console.log("\x1b[0m\x1b[1m\x1b[33mAttachments sent: \x1b[0m\x1b[36m" + (attachments_txt.split("\n").length-1))
     console.log("\x1b[0m\x1b[1m\x1b[33mCharacters per message: \x1b[0m\x1b[36m" + (messages_full.map(m=>m.message).join("").length / total).toFixed(1))
     console.log("\x1b[0m\x1b[1m\x1b[33mMessages per channel: \x1b[0m\x1b[36m" + (total / Object.keys(data).length).toFixed(1))
     console.log("\x1b[0m\x1b[1m\x1b[33mTotal channels: \x1b[0m\x1b[36m" + Object.keys(data).length)
